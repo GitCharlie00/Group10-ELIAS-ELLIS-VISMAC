b33c54c95a5d19b6e49dcfc6c8fda57bef79898f
vis-t10u <vis-t10u@gpu007.cm.cluster>
2025-01-28 19:20:38 +0100

    Test imgs generated

---
+++ generated_imgs.py
@@ -0,0 +1,16 @@
+import torch
+from diffusers import FluxPipeline
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16).to("cuda")
+
+prompt = "A cat holding a sign that says hello world"
+image = pipe(
+    prompt,
+    height=1024,
+    width=1024,
+    guidance_scale=3.5,
+    num_inference_steps=50,
+    max_sequence_length=512,
+    generator=torch.Generator("cuda").manual_seed(0)
+).images[0]
+image.save("flux-dev.png")
