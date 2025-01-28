926692339a353a8f6246c2010af1f492e793ea89
Claudio <schiavella.1884561@studenti.uniroma1.it>
2025-01-28 21:13:54 +0100

    Generated images first run

--- generated_imgs.py
+++ generated_imgs.py
@@ -3,8 +3,6 @@ import torch
 from diffusers import FluxPipeline
 
 file_path = "Unbiased_text_descriptions.txt"
-output_folder = "generated_images"
-os.makedirs(output_folder, exist_ok=True)
 
 pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16).to("cuda")
 
@@ -12,18 +10,21 @@ pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=
 with open(file_path, "r", encoding="utf-8") as f:
     prompts = [line.strip() for line in f.readlines() if line.strip()]
 
-for i, prompt in enumerate(prompts):
-    image = pipe(
-        prompt,
-        height=1024,
-        width=1024,
-        guidance_scale=3.5,
-        num_inference_steps=50,
-        max_sequence_length=512,
-        generator=torch.Generator("cuda").manual_seed(0)
-    ).images[0]
+for j in range(15):
+    output_folder = "generated_images" + str(j)
+    os.makedirs(output_folder, exist_ok=True)
+    for i, prompt in enumerate(prompts):
+        image = pipe(
+            prompt,
+            height=1024,
+            width=1024,
+            guidance_scale=3.5,
+            num_inference_steps=50,
+            max_sequence_length=512,
+            generator=torch.Generator("cuda").manual_seed(0)
+        ).images[0]
 
-    # Salvataggio immagine
-    sanitized_prompt = "_".join(prompt.split())[:100]
-    output_path = os.path.join(output_folder, f"image_{i+1}_{sanitized_prompt}.png")
-    image.save(output_path)
+        # Salvataggio immagine
+        sanitized_prompt = "_".join(prompt.split())[:100]
+        output_path = os.path.join(output_folder, f"image_{i+1}_{sanitized_prompt}.png")
+        image.save(output_path)
