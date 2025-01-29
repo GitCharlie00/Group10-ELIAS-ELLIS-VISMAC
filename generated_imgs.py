f094f4652e6ec2315c1353f73506255e9e56adb9
Claudio <schiavella.1884561@studenti.uniroma1.it>
2025-01-29 10:42:33 +0100

    Different seed generating test

--- generated_imgs.py
+++ generated_imgs.py
@@ -14,18 +14,19 @@ for j in range(15):
     output_folder = "generated_images" + str(j+100)
     os.makedirs(output_folder, exist_ok=True)
     for i, prompt in enumerate(prompts):
-        image = pipe(
-            prompt,
-            height=1024,
-            width=1024,
-            guidance_scale=3.5,
-            num_inference_steps=50,
-            max_sequence_length=512,
-            generator=torch.Generator("cuda").manual_seed((j+1)*1000)
-        ).images[0]
+        if ":" not in prompt:
+            image = pipe(
+                prompt,
+                height=1024,
+                width=1024,
+                guidance_scale=3.5,
+                num_inference_steps=50,
+                max_sequence_length=512,
+                generator=torch.Generator("cuda").manual_seed((j+1)*1000)
+            ).images[0]
 
-        # Salvataggio immagine
-        sanitized_prompt = "_".join(prompt.split())[:100]
-        output_path = os.path.join(output_folder, f"image_{i+1}_{sanitized_prompt}.png")
-        image.save(output_path)
-        exit(0)
+            # Salvataggio immagine
+            sanitized_prompt = "_".join(prompt.split())[:100]
+            output_path = os.path.join(output_folder, f"image_{i+1}_{sanitized_prompt}.png")
+            image.save(output_path)
+            exit(0)
