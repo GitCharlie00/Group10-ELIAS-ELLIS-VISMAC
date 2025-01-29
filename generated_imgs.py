b98a95c18236281902a3e5c4c9584f3a4f76cba4
Claudio <schiavella.1884561@studenti.uniroma1.it>
2025-01-29 10:40:08 +0100

    Generation test with different seed

--- generated_imgs.py
+++ generated_imgs.py
@@ -11,7 +11,7 @@ with open(file_path, "r", encoding="utf-8") as f:
     prompts = [line.strip() for line in f.readlines() if line.strip()]
 
 for j in range(15):
-    output_folder = "generated_images" + str(j)
+    output_folder = "generated_images" + str(j+100)
     os.makedirs(output_folder, exist_ok=True)
     for i, prompt in enumerate(prompts):
         image = pipe(
@@ -21,10 +21,11 @@ for j in range(15):
             guidance_scale=3.5,
             num_inference_steps=50,
             max_sequence_length=512,
-            generator=torch.Generator("cuda").manual_seed(0)
+            generator=torch.Generator("cuda").manual_seed((j+1)*1000)
         ).images[0]
 
         # Salvataggio immagine
         sanitized_prompt = "_".join(prompt.split())[:100]
         output_path = os.path.join(output_folder, f"image_{i+1}_{sanitized_prompt}.png")
         image.save(output_path)
+        exit(0)
